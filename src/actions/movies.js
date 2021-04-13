0457c9f3296877e6dca0770300352ca9a292dbe3
Jacob Short <69212644+JacobShortKenzie@users.noreply.github.com>
2021-04-13 14:56:56 -0700

    started dashboard with bethany

---
+++ src/actions/movies.js
@@ -0,0 +1,29 @@
+// import axios from "axios";
+
+// const moviesApi = "https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/"
+
+// Load initial movies
+
+// export const initialMovies = () => async (dispatch) => {
+//   const starter = {
+//     method: "GET",
+//     url:
+//       "https://ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com/entertainment/match/",
+//     params: { Title: '"Lion King"', ProgramType: '"Movie"' },
+//     headers: {
+//       "content-type": "application/json",
+//       "x-rapidapi-key": "529528fe49mshb79e1f661d36214p1d26d5jsn10cb7da958c2",
+//       "x-rapidapi-host":
+//         "ivaee-internet-video-archive-entertainment-v1.p.rapidapi.com",
+//     },
+//   };
+
+//   await axios
+//     .request(starter)
+//     .then(function (response) {
+//       console.log(response.data);
+//     })
+//     .catch(function (error) {
+//       console.error(error);
+//     });
+// };
--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,19 @@
-import React from "react";
+import Reac, { Fragment } from "react";
 import PropTypes from "prop-types";
+import { initialMovies } from "../../actions/movies";
 
 const Dashboard = (props) => {
-  return <div>Dashboard</div>;
+  return (
+    <>
+      <div className="dashboard">Dashboard</div>
+      <div className="search">
+        <input type="text" placeholder="ex: Lion King" />
+        <button>Search</button>
+        <input type="text" placeholder="ex: Movie" />
+        <button>Search</button>
+      </div>
+    </>
+  );
 };
 
 Dashboard.propTypes = {};
