381334422d5f146ce8ecfa92924a735ffa0c5057
Jacob Short <69212644+JacobShortKenzie@users.noreply.github.com>
2021-04-19 11:02:21 -0700

    added rebekas photo to the app

--- src/components/layout/Base.js
+++ src/components/layout/Base.js
@@ -3,19 +3,21 @@ import { Link } from "react-router-dom";
 
 const Base = () => {
   return (
-    <section>
-      <div>
-        <h1>Welcome to MMG!</h1>
-      </div>
-      <div>
-        <Link to="/login">
-          <button>Login</button>
-        </Link>
-        <Link to="/register">
-          <button>Register</button>
-        </Link>
-      </div>
-    </section>
+    <div>
+      <section>
+        <div>
+          <h1>Welcome to MMG!</h1>
+        </div>
+        <div>
+          <Link to="/login">
+            <button>Login</button>
+          </Link>
+          <Link to="/register">
+            <button>Register</button>
+          </Link>
+        </div>
+      </section>
+    </div>
   );
 };
 
