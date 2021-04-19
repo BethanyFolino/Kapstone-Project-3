3f8e8d00188f23a7827d7cb82ff9b03c3f1145af
Jacob Short <69212644+JacobShortKenzie@users.noreply.github.com>
2021-04-18 17:45:50 -0700

    try this

--- src/components/layout/ImageCard.js
+++ src/components/layout/ImageCard.js
@@ -5,13 +5,14 @@ const baseURL = "https://image.tmdb.org/t/p/original/";
 function ImageCard({ movie }) {
   return (
     <div className="image-card">
-      <img
+      <h1>Hello</h1>
+      {/* <img
         src={`${baseURL}${movie.backdrop_path || movie.poster_path} `}
         alt="movie poster"
-      />
-      <p>{movie.overview}</p>
-      <h2>{movie.title || movie.original_name}</h2>
-      <p>{movie.vote_count}</p>
+      /> */}
+      {/* <p>{movie.overview}</p> */}
+      {/* <h2>{movie.title || movie.original_name}</h2> */}
+      {/* <p>{movie.vote_count}</p> */}
     </div>
   );
 }
