0f1fce72ed0a491bb222706c3eaa15526ace58bf
BETHANY FOLINO <bethany.folino@gmail.com>
2021-04-18 17:42:53 -0700

    added to Profile.js with Jacob

--- src/components/layout/Profile.js
+++ src/components/layout/Profile.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profile";
@@ -7,16 +7,73 @@ const Profile = ({ getCurrentProfile, auth: { user }, profile }) => {
   useEffect(() => {
     getCurrentProfile();
   }, []);
+
+  const [formData, setFormData] = useState({
+    bio: "",
+    favoritemovie: "",
+    favoritegame: "",
+    favoritetvseries: "",
+  });
+
+  const { bio, favoritemovie, favoritegame, favoritetvseries } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
   return (
     <Fragment>
       <h1>Profile</h1>
       <p>Welcome {user && user.name}</p>
-      {user.favoritemovie}
-      {profile !== null ? (
+      {/* {user.favoritemovie} */}
+      {/* {profile !== null ? (
         <Fragment>has</Fragment>
-      ) : (
-        <Fragment>You have not yet set up a profile</Fragment>
-      )}
+      ) : ( */}
+      <Fragment>
+        <h1>Create your profile!</h1>
+        <p>
+          Enter your favorite movies, games, and tv series; and a fun fact about
+          yourself.
+        </p>
+        <small>* = required field</small>
+        <form className="form">
+          <div className="form-group">
+            <textarea
+              placeholder="Your bio here"
+              name="bio"
+              value={bio}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Your favorite movies"
+              name="favoritemovie"
+              value={favoritemovie}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Your favorite games"
+              name="favoritegame"
+              value={favoritegame}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              placeholder="Your favorite TV series"
+              name="favoritetvseries"
+              value={favoritetvseries}
+              onChange={(e) => onChange(e)}
+            />
+          </div>
+        </form>
+        <input type="submit" className="btn btn-primary" />
+      </Fragment>
     </Fragment>
   );
 };
