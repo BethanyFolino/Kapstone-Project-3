9cf059996aa208926c3e5bbb8649ecdea8774a8e
BETHANY FOLINO <bethany.folino@gmail.com>
2021-04-21 12:35:22 -0700

    Changed part of Movie.js and added Rebekah's changes to App.cssand About.js

--- src/components/layout/About.js
+++ src/components/layout/About.js
@@ -2,12 +2,40 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>Bethany</h1>
-      <h1>Rebekah</h1>
-      <h1>Jacob</h1>
-      <h1>Nathan</h1>
-    </div>
+    <div className="about">
+    <div className="about-item">
+     <h1>Bethany Folino</h1>
+     <p>Bethany is a software developer interested in functionality and making
+        apps that simultaneously entertain and help people. She enjoys playing games like
+        Animal Crossing, Hollow Knight, and Super Smash Bros. A few of her favorite
+        TV series are Psych, Doctor Who, and Steins;Gate. Some of her favorite movies 
+        are Now You See Me, Spiderman: Enter the Spider-verse, and the Back to the Future
+        trilogy.</p>
+   </div>
+   <div className="about-item">
+     <h1>Rebekah Miller</h1>
+     <p>Rebekah is a Software Developer interested in accessibility for the
+        visually impaired. She enjoys playing Super Mario Bros. and watching Batman movies.
+        This application features images with alt tags, which permit a screen reader to 
+        tell a visually impaired user what is contained in the image.</p>
+   </div>
+   <div className="about-item">
+     <h1>Jacob Short</h1>
+     <p>Jacob is a young and inspiring software developer, aimed directly at the 
+       functionality and the structure of the application. He spends his free time playing
+       Call of Duty and watching intense dramas whether TV Series, or Movies.
+       This application features a back end utilizing MongoDB, to be able to store
+       our users data which makes it easier for us as devs to handle and store our
+       user's precious and valuable information.</p>
+   </div>
+   <div className="about-item">
+     <h1>Nathan Jolteus</h1>
+     <p>Nathan is an aspiring software developer, hoping to do quite a bit
+        of front end web development. He spends his free time playing Mobile Legends
+        and watching funny YouTube videos. This application features bootstrap, which 
+        adds flavor the application and makes it more visually appealing.</p>
+   </div>
+ </div>
   );
 };
 
--- src/components/layout/Movie.js
+++ src/components/layout/Movie.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Card } from 'react-bootstrap';
+import { Card } from "react-bootstrap";
 
 import Reviews from "../reviews/Reviews";
 import ReviewForm from "../reviews/ReviewForm";
@@ -13,8 +13,6 @@ const Movie = ({ Title, Year, Poster, imdbID }) => {
       setLikes(likes + 1);
     }
   };
-
-
 
   return (
     <>
@@ -30,26 +28,22 @@ const Movie = ({ Title, Year, Poster, imdbID }) => {
       {/* reviews - imdbID exists here */}
       {/* <ReviewForm imdbID={imdbID} />
       <Reviews imdbID={imdbID} />  */}
-      <Card className='our-movies'style={{ width: '18rem' }}>
-  <Card.Img variant="top" src={Poster} />
-  <Card.Body>
-    <Card.Title>Title: {Title}</Card.Title>
-    <Card.Text>
-     Year Created: {Year}
-    </Card.Text>
-    <Card.Text>
-      Likes: {likes}
-    </Card.Text>
-    <button onClick={likeTheMovie()}>Like</button>
-    <Card.Text>
-    <ReviewForm imdbID={imdbID} />
-    </Card.Text>
-    <Card.Text>
-      <h2>Reviews:</h2>
-    <Reviews imdbID={imdbID} />
-    </Card.Text>
-  </Card.Body>
-</Card>
+      <Card className="our-movies" style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={Poster} />
+        <Card.Body>
+          <Card.Title>Title: {Title}</Card.Title>
+          <Card.Text>Year Created: {Year}</Card.Text>
+          <Card.Text>Likes: {likes}</Card.Text>
+          <button onClick={likeTheMovie}>Like</button>
+          <Card.Text>
+            <ReviewForm imdbID={imdbID} />
+          </Card.Text>
+          <Card.Text>
+            <h2>Reviews:</h2>
+            <Reviews imdbID={imdbID} />
+          </Card.Text>
+        </Card.Body>
+      </Card>
     </>
   );
 };
